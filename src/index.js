38d986569a4a0fc2bb917ca40dec5435a8b9e8b3
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-07-05 09:47:27 +0300

    version 1.0.3 use hash router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <BrowserRouter>
+    <HashRouter>
       <CssBaseline />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
